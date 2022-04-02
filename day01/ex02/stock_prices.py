35eb38a3e19a43dae85cca976e7bb4c40c8a974d
Hazzea Charlsie <hcharlsi@ga-a4.msk.21-school.ru>
2022-04-02 15:14:01 +0300

    day01 ex02

---
+++ day01/ex02/stock_prices.py
@@ -0,0 +1,42 @@
+import sys
+
+
+COMPANIES = {
+    'Apple': 'AAPL',
+    'Microsoft': 'MSFT',
+    'Netflix': 'NFLX',
+    'Tesla': 'TSLA',
+    'Nokia': 'NOK'
+}
+
+
+STOCKS = {
+    'AAPL': 287.73,
+    'MSFT': 173.79,
+    'NFLX': 416.90,
+    'TSLA': 724.88,
+    'NOK': 3.37
+}
+
+
+def stock_prices():
+    if len(sys.argv) != 2:
+        return
+    input = sys.argv[1].lower()
+    cmp_name = ''
+    for i in range(len(input)):
+        if i == 0:
+            cmp_name += input[i].upper()
+        else:
+            cmp_name += input[i]
+    cmps_names = [*COMPANIES]
+    if cmp_name not in cmps_names:
+        print('Unknown company')
+        return
+    cmp_stock_name = COMPANIES[cmp_name]
+    print(STOCKS[cmp_stock_name]) 
+    
+
+
+if __name__ == '__main__':
+    stock_prices()
