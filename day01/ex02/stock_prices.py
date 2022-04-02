3930238e4174a5a15eb59c8c4a3ddb8c9dc23219
Hazzea Charlsie <hcharlsi@ga-a4.msk.21-school.ru>
2022-04-02 15:22:06 +0300

    day01 ex02 fix dict location

--- day01/ex02/stock_prices.py
+++ day01/ex02/stock_prices.py
@@ -1,25 +1,21 @@
 import sys
 
 
-COMPANIES = {
-    'Apple': 'AAPL',
-    'Microsoft': 'MSFT',
-    'Netflix': 'NFLX',
-    'Tesla': 'TSLA',
-    'Nokia': 'NOK'
-}
-
-
-STOCKS = {
-    'AAPL': 287.73,
-    'MSFT': 173.79,
-    'NFLX': 416.90,
-    'TSLA': 724.88,
-    'NOK': 3.37
-}
-
-
 def stock_prices():
+    COMPANIES = {
+        'Apple': 'AAPL',
+        'Microsoft': 'MSFT',
+        'Netflix': 'NFLX',
+        'Tesla': 'TSLA',
+        'Nokia': 'NOK'
+    }
+    STOCKS = {
+        'AAPL': 287.73,
+        'MSFT': 173.79,
+        'NFLX': 416.90,
+        'TSLA': 724.88,
+        'NOK': 3.37
+    }
     if len(sys.argv) != 2:
         return
     input = sys.argv[1].lower()
