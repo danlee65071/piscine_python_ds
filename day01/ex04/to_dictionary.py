2953078df7c4f7dcae470d462d4a82d198d4e2b7
Hazzea Charlsie <hcharlsi@ga-a4.msk.21-school.ru>
2022-04-02 16:13:28 +0300

    day01 ex04

---
+++ day01/ex04/to_dictionary.py
@@ -0,0 +1,36 @@
+from collections import defaultdict
+
+
+def to_dictionary():
+    list_of_tuples = [
+        ('Russia', '25'),
+        ('France', '132'),
+        ('Germany', '132'),
+        ('Spain', '178'),
+        ('Italy', '162'),
+        ('Portugal', '17'),
+        ('Finland', '3'),
+        ('Hungary', '2'),
+        ('The Netherlands', '28'),
+        ('The USA', '610'),
+        ('The United Kingdom', '95'),
+        ('China', '83'),
+        ('Iran', '76'),
+        ('Turkey', '65'),
+        ('Belgium', '34'),
+        ('Canada', '28'),
+        ('Switzerland', '26'),
+        ('Brazil', '25'),
+        ('Austria', '14'),
+        ('Israel', '12')
+    ]
+    dict_res = defaultdict(list)
+    for el in list_of_tuples:
+        dict_res[el[1]].append(el[0])
+    for key_d, vals_d in dict_res.items():
+        for val in vals_d:
+            print('\'{}\' : \'{}\''.format(key_d, val))
+
+
+if __name__ == '__main__':
+    to_dictionary()
